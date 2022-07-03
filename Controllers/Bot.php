9b8692dd2dc8d89a84c71d269928920e362041ad
zorionten <zorionten@gmail.com>
2022-07-03 23:17:22 +0530

    Encrypted bot token

--- Controllers/Bot.php
+++ Controllers/Bot.php
@@ -1,62 +1,68 @@
 <?php
+
 namespace Controllers;
 
 use Libs\Curl;
 
-class Bot{
+class Bot
+{
     function __construct()
     {
-        $this->url="https://discord.com/api/v10";
+        $this->url = "https://discord.com/api/v10";
     }
-    function index(){
+    function index()
+    {
         print_r($this->config->get("bot"));
         // print_r($this->config->config);
         echo "<a href='/index.php/bot/startauth'> Auth </a>";
         echo "<br/>";
         echo "<a href='/index.php/bot/action'> Action </a>";
     }
-    function action(){
-        $path="/users/@me";
-        echo $this->url.$path.PHP_EOL;
-        $header=[];
-        $agent=$this->config->get("bot")['agent'];
-        // $header=["Authorization: Bot ".$this->config->get("bot")["auth_key"]];
-        $header=["Authorization: Bearer ".$this->config->get("bot")["token"]['access_token']];
-        $data=Curl::call_json($this->url.$path,"GET",null,$header,null);
+    function action()
+    {
+        $path = "/users/@me/guilds";
+        echo $this->url . $path . PHP_EOL;
+        $header = [];
+        $header = [
+            "Authorization: Bot " . base64_decode($this->config->get("bot")["auth_key"]),
+        ];
+        $data = Curl::call_json($this->url . $path, "GET", null, $header, null);
         print_r($data);
     }
-    function startAuth(){
-        $data=[
-            "client_id"=>$this->config->get("bot")['client'],
+    function startAuth()
+    {
+        $data = [
+            "client_id" => $this->config->get("bot")['client'],
             // "client_secret"=>$this->config['bot']['secret'],
-            "grant_type"=>"authorization_code",
-            "redirect_uri"=>$this->config->get('redirect'),
-            "state"=>"The_state",
-            "response_type"=>"code",
-            "permissions"=>8
+            "grant_type" => "authorization_code",
+            "redirect_uri" => $this->config->get('redirect'),
+            "state" => "The_state",
+            "response_type" => "code",
+            "permissions" => 8
         ];
-        $scope="guilds guilds.join guilds.members.read messages.read identify bot";
-        $url="https://discord.com/api/oauth2/authorize?scope=".str_replace("+","%20",urlencode($scope))."&".http_build_query($data);
+        $scope = "guilds guilds.join guilds.members.read messages.read identify bot";
+        $url = "https://discord.com/api/oauth2/authorize?scope=" . str_replace("+", "%20", urlencode($scope)) . "&" . http_build_query($data);
         echo $url;
         header("location: $url");
     }
-    function getToken(){
-        $code=$_GET['code'];
-        $data=[
-            "client_id"=>$this->config->get('bot')['client'],
-            "client_secret"=>$this->config->get('bot')['secret'],
-            "grant_type"=>"authorization_code",
-            "code"=>$code,
-            "redirect_uri"=>$this->config->get('redirect')
+    function getToken()
+    {
+        $code = $_GET['code'];
+        $data = [
+            "client_id" => $this->config->get('bot')['client'],
+            "client_secret" => $this->config->get('bot')['secret'],
+            "grant_type" => "authorization_code",
+            "code" => $code,
+            "redirect_uri" => $this->config->get('redirect')
         ];
-        $url="https://discord.com/api/oauth2/token";
-        $token=Curl::call_json($url,"POST",$data);
-        $config=$this->config->get('bot');
-        $guild_id=$token['guild']['id'];
-        $token['guild']=[];
-        $token['guild'][]=$guild_id;
-        $config['token']=$token;
-        $this->config->set("bot",$config);
+        $url = "https://discord.com/api/oauth2/token";
+        $token = Curl::call_json($url, "POST", $data);
+        $config = $this->config->get('bot');
+        $guild_id = $token['guild']['id'];
+        $token['guild'] = [];
+        $token['guild'][] = $guild_id;
+        $config['token'] = $token;
+        $this->config->set("bot", $config);
         print_r($token);
         header("location: /index.php/bot/index");
     }
